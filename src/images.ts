af9b52a14d39addd4d2f724bcfdd27c7209fe67e
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-02-20 14:13:15 +0000

    Added mock images

---
+++ src/images.ts
@@ -0,0 +1,7 @@
+export const Logos = {
+  LogoSmall: 'http://localhost:8080/files/eea24904-a6ea-42c9-b05b-88fe98d7c1ec',
+};
+
+export const Headers = {
+  ManchesterLarge: 'http://localhost:8080/files/af339b7d-4885-4db8-9f22-56cf85c1c181',
+};
